06216a7be9f5be4aac7bcaf0a34c78acdef9d61c
Finn McMillan <finnmcm@u.northwestern.edu>
2025-02-18 21:59:36 -0600

    Initial Commit

---
+++ DTCProject/DTCProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTCProjectApp.swift
+//  DTCProject
+//
+//  Created by Finn McMillan on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct DTCProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DTCProjectTests/DTCProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  DTCProjectTests.swift
+//  DTCProjectTests
+//
+//  Created by Finn McMillan on 2/18/25.
+//
+
+import Testing
+@testable import DTCProject
+
+struct DTCProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
